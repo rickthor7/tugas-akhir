2483f4b0916ebbb8af1a3c5bd8698102b6b901d5
erik <erikanin500@gmail.com>
2025-10-15 23:04:56 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+  var ul = document.querySelector('#ul');
+  var li = document.querySelectorAll('#li');
+  
+  li.forEach(el=>{
+      el.addEventListener('click', function(){
+          ul.querySelector('.active').classList.remove('active');
+  
+          el.classList.add('active');
+      });
+  });
+
+
+const swiper = new Swiper('.swiper', {
+    autoplay:{
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    
+    loop: true,
+  
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
+
+
